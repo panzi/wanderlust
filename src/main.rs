6c30865cd735e0a2d7557ddc06583a929f86037c
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2025-05-20 04:19:51 +0200

    demo what is there

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use dialects::postgresql::PostgreSQLParser;
+use migration::generate_migration;
 use model::syntax::Parser;
 
 pub mod model;
@@ -7,6 +8,8 @@ pub mod error;
 pub mod format;
 pub mod migration;
 
+use model::words::*;
+
 fn main() {
     let mut args = std::env::args_os();
     args.next().unwrap();
@@ -14,8 +17,32 @@ fn main() {
     let old = args.next().unwrap();
     let old_source = std::fs::read_to_string(&old).unwrap();
     let mut parser = PostgreSQLParser::new(&old_source);
+
     match parser.parse() {
-        Ok(ddl) => println!("{ddl:#?}\n\n{ddl}"),
+        Ok(old_ddl) => {
+            if let Some(new) = args.next() {
+                let new_source = std::fs::read_to_string(&new).unwrap();
+                let mut parser = PostgreSQLParser::new(&new_source);
+
+                match parser.parse() {
+                    Ok(new_ddl) => {
+                        let migrations = generate_migration(&old_ddl, &new_ddl);
+
+                        println!("{BEGIN};");
+                        for stmt in &migrations {
+                            println!("{stmt}");
+                        }
+                        println!("{COMMIT};");
+                    }
+                    Err(err) => {
+                        err.print(&new.to_string_lossy(), &new_source, &mut std::io::stderr()).unwrap();
+                        std::process::exit(1);
+                    }
+                }
+            } else {
+                println!("{old_ddl}");
+            }
+        }
         Err(err) => {
             err.print(&old.to_string_lossy(), &old_source, &mut std::io::stderr()).unwrap();
             std::process::exit(1);
